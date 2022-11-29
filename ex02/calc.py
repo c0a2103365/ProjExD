f9981b64b01d507205064d299142ad228c20860c
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-11-29 14:37:40 +0900

    問題5が終わりました。

--- ex02/calc.py
+++ ex02/calc.py
@@ -14,7 +14,6 @@ root.geometry("300x500")
 
 
 entry = tk.Entry(root, justify="right", width=10, font=("", 40))
-
 entry.grid(row=0, column=0, columnspan=3)
 
 
@@ -25,6 +24,16 @@ for num in range(9, -1, -1):
                        width=4, height=2, font=("", "30", ""))
     button.grid(row=r, column=c)
     button.bind("<1>", button_click)
+    c += 1
+    if c % 3 == 0:
+        r += 1
+        c = 0
+
+operators = ["+", "="]
+for ope in operators:
+    button = tk.Button(root, text=f"{ope}",
+                       width=4, height=2, font=("", "30", ""))
+    button.grid(row=r, column=c)
     c += 1
     if c % 3 == 0:
         r += 1
