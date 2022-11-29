3def961b8aec3b4ad08b1d645c3a7edf7dc735f6
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-11-29 14:26:44 +0900

    コメント行を消しました。

--- ex02/calc.py
+++ ex02/calc.py
@@ -12,18 +12,6 @@ root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
 
-"""
-button0 = tk.Button(root, text="0", width=4, height=2, font=("", "30", ""))
-button1 = tk.Button(root, text="1", width=4, height=2, font=("", "30", ""))
-button2 = tk.Button(root, text="2", width=4, height=2, font=("", "30", ""))
-button3 = tk.Button(root, text="3", width=4, height=2, font=("", "30", ""))
-button4 = tk.Button(root, text="4", width=4, height=2, font=("", "30", ""))
-button5 = tk.Button(root, text="5", width=4, height=2, font=("", "30", ""))
-button6 = tk.Button(root, text="6", width=4, height=2, font=("", "30", ""))
-button7 = tk.Button(root, text="7", width=4, height=2, font=("", "30", ""))
-button8 = tk.Button(root, text="8", width=4, height=2, font=("", "30", ""))
-button9 = tk.Button(root, text="9", width=4, height=2, font=("", "30", ""))
-"""
 
 r, c = 0, 0
 
