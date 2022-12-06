2c511eed08f57199e26f54c705e9b6f9d8fe14d7
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-12-06 14:28:20 +0900

    問題3

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,5 +4,9 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+
+    cx, cy = 300, 400
+    tori = tk.PhotoImage(file="./8.png")
+    canvas.create_image(cx, cy, image=tori, tag="kokaton")
     canvas.pack()
     root.mainloop()
