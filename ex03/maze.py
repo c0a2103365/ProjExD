4fc9e763b0994186b5372e9e9aabea50e180663b
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-12-06 14:44:18 +0900

    問題7

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,6 +11,16 @@ def key_up(event):
     key = ""
 
 
+def main_proc():
+    global cx, cy
+    if key == "Up": cy -= 20
+    if key == "Down": cy += 20
+    if key == "Left": cx -= 20
+    if key == "Right": cx += 20
+    canvas.coords("kokaton",cx,cy)
+    root.after(100,main_proc)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -24,4 +34,5 @@ if __name__ == "__main__":
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    main_proc()
     root.mainloop()
