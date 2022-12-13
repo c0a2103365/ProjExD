06b25218b357926bf2e0b693a91458ec8fc11a58
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-12-13 14:45:42 +0900

    問題4-5

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 import sys
 
 
@@ -7,6 +8,7 @@ def main():
     # 練習1
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
+    scrn_rct = scrn_sfc.get_rect()
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
     pgbg_rct = pgbg_sfc.get_rect()
 
@@ -17,13 +19,22 @@ def main():
     tori_rct.center = 400, 300
     scrn_sfc.blit(tori_sfc, tori_rct)
 
+    # 練習5
+    bomb_sfc = pg.Surface((20, 20))  # 正方形の空のSruface
+    bomb_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0, scrn_rct.width)
+    bomb_rct.centery = random.randint(0, scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct)
+
     # 練習2
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
-
+        # 練習4
         key_dct = pg.key.get_pressed()  # 辞書型
         if key_dct[pg.K_UP]:
             tori_rct.centery -= 1
@@ -34,7 +45,7 @@ def main():
         if key_dct[pg.K_RIGHT]:
             tori_rct.centerx += 1
         scrn_sfc.blit(tori_sfc, tori_rct)
-
+        scrn_sfc.blit(bomb_sfc,bomb_rct)
 
         pg.display.update()
         clock.tick(1000)
