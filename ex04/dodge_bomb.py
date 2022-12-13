cac3e00f135d6a77bcc9aa21cdd565e9a56b248d
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-12-13 14:17:04 +0900

    問題4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,12 +8,20 @@ def main():
     pg.display.set_caption("逃げろ!こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
-    pgbg_sfc = pg.transform.rotozoom(pgbg_sfc, 0, 2.0)
     pgbg_rct = pgbg_sfc.get_rect()
-    pgbg_rct.center = 400, 300
 
-    pg.display.update()
-    clock.tick(0.5)
+    # 練習2
+    while True:
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        pg.display.update()
+        clock.tick(1000)
+    """
+    pgbg_sfc = pg.transform.rotozoom(pgbg_sfc, 0, 2.0)
+    pgbg_rct.center = 400, 300
+    """
 
 
 if __name__ == "__main__":
