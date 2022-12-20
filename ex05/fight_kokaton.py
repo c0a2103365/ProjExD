a5fc4c3982de5f4ba4322ecc2372148ff4534e22
S_Kanemitsu <c0a2103365@edu.teu.ac.jp>
2022-12-20 15:36:20 +0900

    ボールの色をランダムに変更できる機能を実装

---
+++ ex05/fight_kokaton.py
@@ -0,0 +1,127 @@
+import pygame as pg
+import random
+import sys
+
+
+class Screen:
+    def __init__(self, title, wh, img_path):
+        # 練習1
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh)
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(img_path)
+        self.bgi_rct = self.bgi_sfc.get_rect()
+
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+
+
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img_path, ratio, xy):
+        self.sfc = pg.image.load(img_path)
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr):
+        key_dct = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+            if check_bound(self.rct, scr.rct) != (+1, +1):
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)
+
+
+class Bomb:
+    def __init__(self, color, rad, vxy, scr: Screen):
+        self.sfc = pg.Surface((2*rad, 2*rad))  # 正方形の空のSurface
+        self.sfc.set_colorkey((0, 0, 0))
+        pg.draw.circle(self.sfc, color, (rad, rad), rad)
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = random.randint(0, scr.rct.width)
+        self.rct.centery = random.randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+
+    def blit(self, scr: Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr: Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        self.sfc.blit(self.sfc, self.rct)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+def check_bound(obj_rct, scr_rct):
+    """
+    第1引数：こうかとんrectまたは爆弾rect
+    第2引数：スクリーンrect
+    範囲内：+1／範囲外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = -1
+    return yoko, tate
+
+
+def main():
+    clock = pg.time.Clock()
+
+    # 練習1
+    scr = Screen("逃げろこうかとん", (1600, 900), "fig/pg_bg.jpg")
+
+    # 練習３
+    kkt = Bird("fig/6.png", 2.0, (900, 400))
+    kkt.update(scr)
+
+    # 練習５
+    bkd_lst = []
+    for _ in range(5):
+        # 追加機能:ボールの色が起動するごとに変わる
+        # 3原色を管理するタプル（内包表記）
+        random_color_tpl = (random.randint(0, 256) for _ in range(3))
+        color1, color2, color3 = random_color_tpl
+        bkd = Bomb((color1, color2, color3), 10, (1, 1), scr)
+        bkd_lst.append(bkd)
+    # bkd.update(scr)
+    # 練習２
+    while True:
+        scr.blit()
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+
+        kkt.update(scr)
+        for i in range(5):
+            bkd_lst[i].update(scr)
+            bkd.update(scr)
+            if kkt.rct.colliderect(bkd_lst[i].rct):
+                return
+
+        pg.display.update()
+        clock.tick(100)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
